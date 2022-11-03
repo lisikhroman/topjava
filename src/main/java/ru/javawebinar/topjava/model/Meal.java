1668fa29ffdbe3ebc4e4cb55d12052f8745d6d2e
DESKTOP-CUGMPBP\79234 <Roman.Lisikh@indusoft.ru>
2022-11-03 14:55:14 +0700

    implement HW4

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,19 +1,45 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import lombok.*;
+import org.hibernate.validator.constraints.Range;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-
+@NamedQueries(value = {
+        @NamedQuery(name = Meal.DELETE, query = "DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:userId"),
+        @NamedQuery(name = Meal.GET, query = "SELECT m FROM Meal m WHERE m.id=:id AND m.user.id=:userId"),
+        @NamedQuery(name = Meal.GET_BETWEEN, query = "SELECT m FROM Meal m WHERE m.user.id=?1 AND m.dateTime>=?2 AND m.dateTime<?3 ORDER BY m.dateTime DESC"),
+        @NamedQuery(name = Meal.GET_ALL, query = "SELECT m FROM Meal m WHERE m.user.id=:userId ORDER BY m.dateTime DESC"),
+})
+@Entity
+@Table(name = "meals")
+@ToString
+@Getter
+@Setter
 public class Meal extends AbstractBaseEntity {
+
+    public static final String DELETE = "Meal.delete";
+    public static final String GET = "Meal.get";
+    public static final String GET_BETWEEN = "Meal.getBetweenHalfOpen";
+    public static final String GET_ALL = "Meal.getAll";
+    @Column(name = "date_time", nullable = false, columnDefinition = "timestamp default now()", updatable = false)
+    @NotNull
     private LocalDateTime dateTime;
-
+    @Column(name = "description", nullable = false)
+    @NotBlank
+    @Size(max = 128)
     private String description;
-
+    @Column(name = "calories", nullable = false, columnDefinition = "int default 0")
+    @Range(min = 10, max = 10000)
     private int calories;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @ToString.Exclude
     private User user;
 
     public Meal() {
@@ -30,18 +56,18 @@ public class Meal extends AbstractBaseEntity {
         this.calories = calories;
     }
 
-    public LocalDateTime getDateTime() {
-        return dateTime;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public int getCalories() {
-        return calories;
-    }
-
+//    public LocalDateTime getDateTime() {
+//        return dateTime;
+//    }
+//
+//    public String getDescription() {
+//        return description;
+//    }
+//
+//    public int getCalories() {
+//        return calories;
+//    }
+//
     public LocalDate getDate() {
         return dateTime.toLocalDate();
     }
@@ -49,19 +75,19 @@ public class Meal extends AbstractBaseEntity {
     public LocalTime getTime() {
         return dateTime.toLocalTime();
     }
-
-    public void setDateTime(LocalDateTime dateTime) {
-        this.dateTime = dateTime;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setCalories(int calories) {
-        this.calories = calories;
-    }
-
+//
+//    public void setDateTime(LocalDateTime dateTime) {
+//        this.dateTime = dateTime;
+//    }
+//
+//    public void setDescription(String description) {
+//        this.description = description;
+//    }
+//
+//    public void setCalories(int calories) {
+//        this.calories = calories;
+//    }
+//
     public User getUser() {
         return user;
     }
@@ -70,13 +96,13 @@ public class Meal extends AbstractBaseEntity {
         this.user = user;
     }
 
-    @Override
-    public String toString() {
-        return "Meal{" +
-                "id=" + id +
-                ", dateTime=" + dateTime +
-                ", description='" + description + '\'' +
-                ", calories=" + calories +
-                '}';
-    }
+//    @Override
+//    public String toString() {
+//        return "Meal{" +
+//                "id=" + id +
+//                ", dateTime=" + dateTime +
+//                ", description='" + description + '\'' +
+//                ", calories=" + calories +
+//                '}';
+//    }
 }
